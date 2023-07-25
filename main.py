0f4811b6a3d7a7e0449fb62acb6f9bd4b8cee5e0
Avraham Natan <avinat123@gmail.com>
2023-07-25 11:11:29 +0300

    tidy up the main

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ if __name__ == '__main__':
     print(f'Hi MGSD malogistics pipeline!')
     start_time = datetime.now()
 
-    domain_file_path = Path("benchmarks_mal/logistics_combined_benchmarks")
-    problem_file_path = Path("benchmarks_mal/logistics_combined_benchmarks")
-    combined_plan_file_path = Path("benchmarks_mal/logistics_combined_benchmarks")
+    domain_file_path = Path("benchmarks_mal/logistics_combined_benchmarks/logistics_combined_domain.pddl")
+    problem_file_path = Path("benchmarks_mal/logistics_combined_benchmarks/logistics_combined_problem.pddl")
+    combined_plan_file_path = Path("benchmarks_mal/logistics_combined_benchmarks/logistics_combined_plan.solution")
+    combined_trajectory_file_path = Path("benchmarks_mal/logistics_combined_benchmarks/logistics_combined_triplets.trajectory")
 
-    combined_domain = DomainParser(domain_path=domain_file_path / "logistics_combined_domain.pddl", partial_parsing=False).parse_domain()
-    combined_problem = ProblemParser(problem_path=problem_file_path / "logistics_combined_problem.pddl",
-                                     domain=combined_domain).parse_problem()
+    combined_domain = DomainParser(domain_path=domain_file_path, partial_parsing=False).parse_domain()
+    combined_problem = ProblemParser(problem_path=problem_file_path, domain=combined_domain).parse_problem()
     trajectory_exporter = MultiAgentTrajectoryExporter(combined_domain)
-    triplets = trajectory_exporter.parse_plan(problem=combined_problem, plan_path=combined_plan_file_path / "logistics_concurrent_plan.solution")
-    trajectory_exporter.export_to_file(triplets, problem_file_path / f"logistics_combined_triplets.trajectory")
+    triplets = trajectory_exporter.parse_plan(problem=combined_problem, plan_path=combined_plan_file_path)
+    trajectory_exporter.export_to_file(triplets, combined_trajectory_file_path)
 
     end_time = datetime.now()
     delta = end_time - start_time
